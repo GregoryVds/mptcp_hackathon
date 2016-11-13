b60582ee28911fd686f705a1f959b56e15d3f45b
Gregory Vander Schueren <gregory.vanderschueren@gmail.com>
2016-11-13 11:29:17 -0800

    Close subflow seems working

--- lib.rb
+++ lib.rb
@@ -1,88 +1,120 @@
 require 'socket'
 require 'ffi'
 
-MPTCP_GET_SUB_IDS=66      # Get subflows ids
-MPTCP_CLOSE_SUB_ID=67     # Close sub id
-MPTCP_GET_SUB_TUPLE=68    # Get sub tuple
-MPTCP_OPEN_SUB_TUPLE=69   # Open sub tuple
-MPTCP_SUB_GETSOCKOPT=71   # Get sockopt for a specific sub
-MPTCP_SUB_SETSOCKOPT=72   # Set sockopt for a specific sub
+module MPTCP
+  extend FFI::Library
+  TCP_PROTO_NUM=6
 
-class SubStatus < FFI::Struct
-  layout :id,         :uint8,
-		     :low_prio,   :uint16 
+  MPTCP_GET_SUB_IDS=66      # Get subflows ids
+  MPTCP_CLOSE_SUB_ID=67     # Close sub id
+  MPTCP_GET_SUB_TUPLE=68    # Get sub tuple
+  MPTCP_OPEN_SUB_TUPLE=69   # Open sub tuple
+  MPTCP_SUB_GETSOCKOPT=71   # Get sockopt for a specific sub
+  MPTCP_SUB_SETSOCKOPT=72   # Set sockopt for a specific sub
+
+  class SubStatus < FFI::Struct
+    layout  :id,         :uint8,
+            :low_prio,   :uint16 
+  end
+
+  class Subs < FFI::Struct
+    layout :sub_count,  :uint8,
+           :sub_status, [SubStatus, 256]
+    # Since we don't know the size of the array in advance, we just set it to
+    # the maximum possible size, which is 2^8 (uint8).
+  end
+
+  def self.get_sub_ids(sock)
+    # In this case, we can use the getsockopt method exposed by Ruby Socket 
+    # class. No need to call underlying original C function.
+    opt = sock.getsockopt(:IPPROTO_TCP, MPTCP_GET_SUB_IDS) 
+    memBuf = FFI::MemoryPointer.new(Subs).put_bytes(0, opt.data)
+    Subs.new(memBuf)
+  end
+
+  ############################################################################
+
+  class MptcpSockaddrIn < FFI::Struct
+    layout  :sin_family,  :short,
+            :sin_port,    :ushort,
+            :sin_addr,    :ulong,
+            :sin_zero,    [:char, 8]
+  end
+
+  class MptcpSockaddrIn6 < FFI::Struct
+    layout  :sin6_family,   :uint16,
+            :sin6_port,     :uint16,
+            :sin6_flowinfo, :uint32,
+            :sin6_addr,     [:uchar, 16],
+            :sin6_scope_id, :uint32
+  end
+
+  class MptcpSockaddr < FFI::Struct
+    layout :sa_family, :ushort,
+           :sa_data,   [:char, 14]
+  end
+
+  class MptcpSubTupleIn6 < FFI::Struct
+    layout  :id,   :uint8,
+            :prio, :uint8,
+            :addr1, MptcpSockaddrIn6,
+            :addr2, MptcpSockaddrIn6
+  end
+
+  class MptcpSubTupleIn < FFI::Struct
+    layout  :id,   :uint8,
+            :prio, :uint8,
+            :addr1, MptcpSockaddrIn,
+            :addr2, MptcpSockaddrIn
+  end
+ 
+  def self.open_subflow(sock, mptcp_sub_tuple)
+    # In this case, we need to use the C getsockopt() function since the 
+    # getsockopt() exposed by Ruby does not allow the optval to be passed as an
+    # argument, but only returns an optval.
+    fd = sock.fileno
+    level = :IPPROTO_TCP
+    optname = MPTCP_OPEN_SUB_TUPLE
+    optval = mptcp_sub_tuple.pointer
+    optlen = mptcp_sub_tuple.size
+    getsockopt(fd, level, optname, optval, optlen) 
+  end
+  
+  ############################################################################
+
+  ffi_lib 'c'
+  attach_function :getsockopt, [ :int, :int, :int, :pointer, :pointer ], :int
+
+  class MptcpCloseSubId < FFI::Struct
+    layout  :id,  :uint8,
+            :how, :int
+  end
+
+  class Socklen_t < FFI::Struct
+    layout :val, :int
+  end
+
+  def self.close_subflow(sock, id, how)
+    optval = MptcpCloseSubId.new 
+    optval[:id] = id
+    optval[:how] = how
+    optlen = Socklen_t.new
+    optlen[:val] = optval.size
+    getsockopt(sock.fileno, TCP_PROTO_NUM, MPTCP_CLOSE_SUB_ID,
+               optval.pointer, optlen.pointer)
+  end
+  
+  ############################################################################  
+  
+  def self.get_sub_tuple(sock)
+  end
+
 end
 
-class Subs < FFI::Struct
-  layout :sub_count,  :uint8,
-		     :sub_status, [SubStatus, 256]
-  # Since we don't know the size of the array in advance, we just set it to
-  # the maximum possible size, which is 2^8 (uint8).
-end
-
-def mptcp_get_sub_ids(sock)
-  opt = sock.getsockopt(:IPPROTO_TCP, MPTCP_GET_SUB_IDS) 
-  memBuf = FFI::MemoryPointer.new(Subs).put_bytes(0, opt.data)
-  Subs.new(memBuf)
-end
-
-class MptcpCloseSubId < FFI::Struct
-  layout  :id,  :uint8,
-          :how, :int
-end
-
-def mptcp_close_sub_id(sock, id, how)
-  opt_val = MptcpCloseSubId.new 
-  opt_val[:id] = id
-  opt_val[:how] = how
-  sockopt = Socket::Option.new(
-    :INET,
-    :IPPROTO_TCP,
-    MPTCP_CLOSE_SUB_ID, 
-    opt_val.pointer.get_bytes(0, opt_val.size) # Verify this works?
-  )
-  # Ok, this is wrong as getsockopt does not accept a socket option obvioulsy...
-  # Damn it -_-
-  opt = sock.getsockopt(sockopt)
-end
-
-class MptcpSockaddrIn < FFI::Struct
-  layout  :sin_family,  :short,
-          :sin_port,    :ushort,
-          :sin_addr,    :ulong,
-          :sin_zero,    [:char, 8]
-end
-
-class MptcpSockaddrIn6 < FFI::Struct
-  layout  :sin6_family,   :uint16,
-          :sin6_port,     :uint16,
-          :sin6_flowinfo, :uint32,
-          :sin6_addr,     [:uchar, 16],
-          :sin6_scope_id, :uint32
-end
-
-class MptcpSockaddr < FFI::Struct
-  layout :sa_family, :ushort,
-         :sa_data,   [:char, 14]
-end
-
-class MptcpSubTupleIn6 < FFI::Struct
-  layout  :id,   :uint8,
-          :prio, :uint8,
-          :addr1, MptcpSockaddrIn6,
-          :addr2, MptcpSockaddrIn6
-end
-
-class MptcpSubTupleIn < FFI::Struct
-  layout  :id,   :uint8,
-          :prio, :uint8,
-          :addr1, MptcpSockaddrIn,
-          :addr2, MptcpSockaddrIn
-end
 
 sock = TCPSocket.new('192.168.99.100', 8000)
-subs = mptcp_get_sub_ids(sock)
+subs = MPTCP.get_sub_ids(sock)
 puts subs[:sub_count]
 puts subs[:sub_status][0][:id]
-mptcp_close_sub_id(sock, 1, 1)
-
+# mptcp_close_sub_id(sock, 1, 1)
+puts MPTCP.close_subflow(sock, 1, 0)
